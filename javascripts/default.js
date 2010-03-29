76b20b7e42a9c2e0f45dde7af59e6b91789726e9
Juriy Zaytsev <kangax@gmail.com>
2010-03-29 14:01:52 -0400

    Teach emile how to handle opacity in IE. Minor fixes to layout in IE6.

--- javascripts/default.js
+++ javascripts/default.js
@@ -1,5 +1,84 @@
 /* http://github.com/madrobby/emile */
-(function(f,a){var h=document.createElement("div"),g=("backgroundColor borderBottomColor borderBottomWidth borderLeftColor borderLeftWidth borderRightColor borderRightWidth borderSpacing borderTopColor borderTopWidth bottom color fontSize fontWeight height left letterSpacing lineHeight marginBottom marginLeft marginRight marginTop maxHeight maxWidth minHeight minWidth opacity outlineColor outlineOffset outlineWidth paddingBottom paddingLeft paddingRight paddingTop right textIndent top width wordSpacing zIndex").split(" ");function e(j,k,l){return(j+(k-j)*l).toFixed(3)}function i(k,j,l){return k.substr(j,l||1)}function c(l,p,s){var n=2,m,q,o,t=[],k=[];while(m=3,q=arguments[n-1],n--){if(i(q,0)=="r"){q=q.match(/\d+/g);while(m--){t.push(~~q[m])}}else{if(q.length==4){q="#"+i(q,1)+i(q,1)+i(q,2)+i(q,2)+i(q,3)+i(q,3)}while(m--){t.push(parseInt(i(q,1+m*2,2),16))}}}while(m--){o=~~(t[m+3]+(t[m]-t[m+3])*s);k.push(o<0?0:o>255?255:o)}return"rgb("+k.join(",")+")"}function b(l){var k=parseFloat(l),j=l.replace(/^[\-\d\.]+/,"");return isNaN(k)?{v:j,f:c,u:""}:{v:k,f:e,u:j}}function d(m){var l,n={},k=g.length,j;h.innerHTML='<div style="'+m+'"></div>';l=h.childNodes[0].style;while(k--){if(j=l[g[k]]){n[g[k]]=b(j)}}return n}a[f]=function(p,m,j){p=typeof p=="string"?document.getElementById(p):p;j=j||{};var r=d(m),q=p.currentStyle?p.currentStyle:getComputedStyle(p,null),l,s={},n=+new Date,k=j.duration||200,u=n+k,o,t=j.easing||function(v){return(-Math.cos(v*Math.PI)/2)+0.5};for(l in r){s[l]=b(q[l])}o=setInterval(function(){var v=+new Date,w=v>u?1:(v-n)/k;for(l in r){p.style[l]=r[l].f(s[l].v,r[l].v,t(w))+r[l].u}if(v>u){clearInterval(o);j.after&&j.after()}},10)}})("emile",this);
+// emile.js (c) 2009 Thomas Fuchs
+// Licensed under the terms of the MIT license.
+
+(function(emile, container){
+  var parseEl = document.createElement('div'),
+    props = ('backgroundColor borderBottomColor borderBottomWidth borderLeftColor borderLeftWidth '+
+    'borderRightColor borderRightWidth borderSpacing borderTopColor borderTopWidth bottom color fontSize '+
+    'fontWeight height left letterSpacing lineHeight marginBottom marginLeft marginRight marginTop maxHeight '+
+    'maxWidth minHeight minWidth opacity outlineColor outlineOffset outlineWidth paddingBottom paddingLeft '+
+    'paddingRight paddingTop right textIndent top width wordSpacing zIndex').split(' '),
+    supportsOpacity = typeof parseEl.style.opacity == 'string', 
+    supportsFilters = typeof parseEl.style.filter == 'string',
+    reOpacity = /alpha\(opacity=([^\)]+)\)/, 
+    setOpacity = function(){ }, 
+    getOpacityFromComputed = function(){ return 1; };
+    
+  if (supportsOpacity) {
+    setOpacity = function(el, value){ el.style.opacity = value; };
+    getOpacityFromComputed = function(computed) { return computed.opacity; };
+  }
+  else if (supportsFilters) {
+    setOpacity = function(el, value){
+      var es = el.style;
+      if (reOpacity.test(es.filter)) {
+        value = value >= 0.9999 ? '' : ('alpha(opacity=' + (value * 100) + ')');
+        es.filter = es.filter.replace(reOpacity, value);
+      }
+      else {
+        es.filter += ' alpha(opacity=' + (value * 100) + ')';
+      }
+    };
+    getOpacityFromComputed = function(comp) {
+      var m = comp.filter.match(reOpacity);
+      return (m ? (m[1] / 100) : 1) + '';
+    };
+  }
+    
+  function interpolate(source,target,pos){ return (source+(target-source)*pos).toFixed(3); }
+  function s(str, p, c){ return str.substr(p,c||1); }
+  function color(source,target,pos){
+    var i = 2, j, c, tmp, v = [], r = [];
+    while(j=3,c=arguments[i-1],i--)
+      if(s(c,0)=='r') { c = c.match(/\d+/g); while(j--) v.push(~~c[j]); } else {
+        if(c.length==4) c='#'+s(c,1)+s(c,1)+s(c,2)+s(c,2)+s(c,3)+s(c,3);
+        while(j--) v.push(parseInt(s(c,1+j*2,2), 16)); }
+    while(j--) { tmp = ~~(v[j+3]+(v[j]-v[j+3])*pos); r.push(tmp<0?0:tmp>255?255:tmp); }
+    return 'rgb('+r.join(',')+')';
+  }
+  
+  function parse(prop){
+    var p = parseFloat(prop), q = prop.replace(/^[\-\d\.]+/,'');
+    return isNaN(p) ? { v: q, f: color, u: ''} : { v: p, f: interpolate, u: q };
+  }
+  
+  function normalize(style){
+    var css, rules = {}, i = props.length, v;
+    parseEl.innerHTML = '<div style="'+style+'"></div>';
+    css = parseEl.childNodes[0].style;
+    while(i--) if(v = css[props[i]]) rules[props[i]] = parse(v);
+    return rules;
+  } 
+  
+  container[emile] = function(el, style, opts){
+    el = typeof el == 'string' ? document.getElementById(el) : el;
+    opts = opts || {};
+    var target = normalize(style), comp = el.currentStyle ? el.currentStyle : getComputedStyle(el, null),
+      prop, current = {}, start = +new Date, dur = opts.duration||200, finish = start+dur, interval,
+      easing = opts.easing || function(pos){ return (-Math.cos(pos*Math.PI)/2) + 0.5; }, curValue;
+    for(prop in target) current[prop] = parse(prop === 'opacity' ? getOpacityFromComputed(comp) : comp[prop]);
+    interval = setInterval(function(){
+      var time = +new Date, pos = time>finish ? 1 : (time-start)/dur;
+      for(prop in target) {
+        curValue = target[prop].f(current[prop].v,target[prop].v,easing(pos)) + target[prop].u;
+        if (prop === 'opacity') setOpacity(el, curValue);
+        else el.style[prop] = curValue;
+      }
+      if(time>finish) { clearInterval(interval); opts.after && opts.after(); }
+    },10);
+  }
+})('emile', this);
 
 /* ga */
 var _gaq = [['_setAccount', 'UA-2029229-5'], ['_trackPageview']];
